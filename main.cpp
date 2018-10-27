682061a77ffe8aef2eefc414c33325ce4ba9721e
Unknown <ada-977@tlen.pl>
2018-10-27 19:28:02 +0200

    Greedy colouring graph algorithm.

--- main.cpp
+++ main.cpp
@@ -29,14 +29,14 @@ int lowest_colour(std::vector<bool> &row, std::vector<int> &result){
     return -1;
 }
 
-std::vector<int> greedy(std::vector<std::vector<bool> > &matrix)
+std::vector<int> greedy(std::vector<std::vector<bool> > &matrix){
     /* -----
     Returns a vector with number of used colours as the first element
     and colours of consecutive vertices.
 
     @matrix - adjacency matrix of input graph
     ----- */
-{   
+
     int colour;
     std::vector<int> result (matrix.size(), 0);
     for (int i = 1; i < matrix.size(); i++){
@@ -48,47 +48,5 @@ std::vector<int> greedy(std::vector<std::vector<bool> > &matrix)
 
 int main(){
     
-    int n;
-    std::cout << "Give the vertices number: ";
-    std::cin >> n;
-    std::cout << "\nGive the values (type -1 if there are no more values): \n";
-    std::vector<std::vector<bool> > graph (n+1);
-
-    //fill the graph with false value
-    for (int i = 0; i < n+1; i++){
-        std::vector<bool> temp2 (n+1, false);
-        graph[i] = temp2;
-    }
-            
-
-
-    int temp[n+1] = {};
-    for (int i = 1; i < n+1; i++){
-        std::cout << "Line number " << i << ": ";
-        for (int j = 1; j < n+1; j++){
-            std::cin >> temp[j];
-        }
-        std::cout << std::endl;
-        //convert given numbers to boolean adjacency matrix
-        for (int j = 1; j < n+1; j++){
-            if (temp[j] != -1)
-                graph[i][temp[j]] = true;
-        }
-    }
-
-    std::cout << "\nEntered graph: " << std::endl;
-    for (int i = 0; i < n+1; i++){
-        for (int j = 0; j < n+1; j++)
-            std::cout << graph[i][j] << " ";
-        std::cout << std::endl;
-    }
-
-    std::vector<int> result = greedy(graph);
-    std::cout << "\nWynik = ";
-    for (int i = 0; i < result.size(); i++)
-        if (i != result.size()-1)
-            std::cout << result[i] << ", ";
-        else
-            std::cout << result[i] << std::endl;
     return 0;
 }
