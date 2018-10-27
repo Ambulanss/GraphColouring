5a7670d6da5a0abbfd1c1322a5350ad2640cd3ae
Unknown <ada-977@tlen.pl>
2018-10-27 19:05:50 +0200

    Added descriptions.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 
 int lowest_colour(std::vector<bool> &row, std::vector<int> &result){
+    /* -----
+    Returns the lowest colour for colouring given vertex.
+
+    @row - row from adjacency matrix, containing neighbours of vertex that is being processed
+    @result - vector containing current number of used colours (at index 0)
+            and colours of vertices (index means a vertex number and value is colour)
+    ----- */
+
     std::vector<bool> possible (result[0]+2, true); //vector of all colours used so far + 1
     for (int i = 1; i < row.size(); i++){
         if (row[i]){ //if a neighbour is found
@@ -11,7 +19,7 @@ int lowest_colour(std::vector<bool> &row, std::vector<int> &result){
         }
     }
     for (int i = 1; i < possible.size(); i++){
-        if (possible[i]){
+        if (possible[i]){ //if a colour is possible to use
             if (i == possible.size()-1){ //if chosen colour is new
                 result[0]++; //then the number of colours must be incremented
             }
@@ -22,8 +30,14 @@ int lowest_colour(std::vector<bool> &row, std::vector<int> &result){
 }
 
 std::vector<int> greedy(std::vector<std::vector<bool> > &matrix)
+    /* -----
+    Returns a vector with number of used colours as the first element
+    and colours of consecutive vertices.
+
+    @matrix - adjacency matrix of input graph
+    ----- */
 {   
-    int colour = -2; //for debugging; change it later
+    int colour;
     std::vector<int> result (matrix.size(), 0);
     for (int i = 1; i < matrix.size(); i++){
         colour = lowest_colour(matrix[i], result);
@@ -33,7 +47,6 @@ std::vector<int> greedy(std::vector<std::vector<bool> > &matrix)
 }
 
 int main(){
-
     
     int n;
     std::cout << "Give the vertices number: ";
@@ -56,11 +69,13 @@ int main(){
             std::cin >> temp[j];
         }
         std::cout << std::endl;
+        //convert given numbers to boolean adjacency matrix
         for (int j = 1; j < n+1; j++){
             if (temp[j] != -1)
                 graph[i][temp[j]] = true;
         }
     }
+
     std::cout << "\nEntered graph: " << std::endl;
     for (int i = 0; i < n+1; i++){
         for (int j = 0; j < n+1; j++)
@@ -71,7 +86,9 @@ int main(){
     std::vector<int> result = greedy(graph);
     std::cout << "\nWynik = ";
     for (int i = 0; i < result.size(); i++)
-        std::cout << result[i] << ", ";
-
+        if (i != result.size()-1)
+            std::cout << result[i] << ", ";
+        else
+            std::cout << result[i] << std::endl;
     return 0;
 }
