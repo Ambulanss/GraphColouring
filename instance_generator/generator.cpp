ae10fe1235cbd997b2223469f2874944e3178e75
Andrzej Dąbrowski <newaadd@gmail.com>
2018-11-14 21:21:24 +0100

    Optimize generator, comment AdjacencyMatrix a bit

--- instance_generator/generator.cpp
+++ instance_generator/generator.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <vector>
 #include <random>
+#include <tuple>
+#include <stdexcept>
+#include <algorithm>
 
 class AdjacencyMatrix{    
     private:
@@ -11,26 +14,23 @@ class AdjacencyMatrix{
 
     public:
         
-        AdjacencyMatrix(int size, int spars)
+        AdjacencyMatrix(unsigned long long int size, int spars)
         {
             sparsity = spars; //minimum sparsity of graph given in number of vertices
-            
-            try
-            {
-                // init matrix with zeros meaning no vertices
-                matrix = std::vector <std::vector <bool> >(size, std::vector<bool>(size)); 
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << '\n';
-            }
-            
-            
-            std::default_random_engine generator;
-            number_of_edges = 0; //holder for current number of vertices
+            size = size + 1; //because vertex indexes start from 1
+                    
+            // init matrix with zeros meaning no vertices
+            matrix = std::vector <std::vector <bool> >(size, std::vector<bool>(size)); 
+
+            //fill matrix with edges according to arguments
+            std::default_random_engine generator; //random number generating engine
+            std::vector<std::tuple<int, int> > unused_edges; //vector of unused edges so far
+            number_of_edges = 0; //holder for current number of vertices, debugging purposes mostly
             int roll = 0; //holder for a random value
-            for(int i = 1; i < size; i++)
+            //make a connected graph first
+            for(unsigned int i = 1; i < matrix.size(); i++)
             {
+                //n operations like this
                 if(i != 1)
                 {
                     std::uniform_int_distribution<int> distribution(1, i-1);
@@ -41,23 +41,35 @@ class AdjacencyMatrix{
                 }
             }
 
-            int maximum_number_of_edges = (size * (size -1))/2;
-            float edge_goal = static_cast<float>(sparsity)/100.0 * static_cast<float>(maximum_number_of_edges);
-            std::cout<<std::endl<<edge_goal<<std::endl;
-            
-            while(number_of_edges < edge_goal)
+            for(int x = 1; x < matrix.size(); x++)
             {
-                std::uniform_int_distribution<int> distribution(1, size - 1);
-                int roll_x = distribution(generator);
-                int roll_y = distribution(generator);
-                if(matrix[roll_x][roll_y] != true)
+                
+                for(int y = 1; y < x; y++)
                 {
-                    matrix[roll_x][roll_y] = true;
-                    matrix[roll_y][roll_x] = true;
-                    number_of_edges++;
+                    //n * (n-1)/2 operations like this
+                    if(matrix[x][y] == false)
+                    {
+                        auto temp = std::make_tuple(x, y);
+                        unused_edges.push_back(temp);
+                    }
                 }
             }
 
+            int maximum_number_of_edges = ((size-1) * (size - 2))/2;// n*(n-1)/2 because graph is undirected
+            float edge_goal = static_cast<float>(sparsity)/100.0 * static_cast<float>(maximum_number_of_edges);
+            std::cout<<std::endl<<edge_goal<<std::endl;
+            
+            std::shuffle(unused_edges.begin(), unused_edges.end(), generator);//linear complexity
+            while(number_of_edges < edge_goal && !unused_edges.empty())
+            {
+                auto tmp = unused_edges.back();
+                matrix[std::get<0>(tmp)][std::get<1>(tmp)] = true;
+                matrix[std::get<1>(tmp)][std::get<0>(tmp)] = true;
+                number_of_edges++;
+                unused_edges.pop_back(); 
+
+            }
+            std::cout<<"Number of edges achieved: "<< number_of_edges<<std::endl;
         }
         
         void print_matrix()
@@ -74,23 +86,21 @@ class AdjacencyMatrix{
         
         void write_to_file(std::string filename)
         {
-            std::ofstream file(filename, std::ofstream::out);
-            std::string output = "";
-            file << matrix.size() - 1<<std::endl;
+            std::ofstream stream(filename, std::ofstream::out);
+            stream << matrix.size() - 1<<std::endl;
             for(unsigned int i = 1; i < matrix.size(); i++)
             {
                 for(unsigned int j = 1; j < i; j++)
                 {
                     if(matrix[i][j] == true)
                     {
-                        file<<j<<" "<<i<<std::endl;
+                        stream<<j<<" "<<i<<std::endl;
                     }
                 }
             }
-            file.close();
-            
+               
         }
-        
+
         std::vector<bool>& operator[](int x) //overloading [] operator to be able to access matrix data from outside
         {
             return matrix[x];
@@ -98,42 +108,60 @@ class AdjacencyMatrix{
 
 };
 
-void parse_and_check_input(int argc, char const *argv[], int & size, int &sparsity, std::string & filename)
+void parse_and_check_input(int argc, char const *argv[], long long int & size, int &sparsity, std::string & filename)
 {
     if(argc != 4){
-        std::cout<<"Usage:\ngenerator.exe <number_of_edges> <sparsity_as_full_percents> <filename>\n";
-        exit(-1);
+        throw "Usage:\ngenerator.exe <number_of_edges> <sparsity_as_full_percents> <filename>\n";
     }
-    size = atoi(argv[1]) + 1;
+    size = atol(argv[1]);
+    std::cout<<"Size: "<< size<<std::endl;
     sparsity = atoi(argv[2]);
+    std::cout<<"Sparsity: "<< sparsity <<std::endl;
     filename = argv[3];
-    if(size < 1)
+
+    if(size < 1 || size > 25000)
     {
-        std::cout<<"Size must be bigger than 0\n";
-        exit(-2);
+        throw "Wrong size: must be from 1 to 25000\n";
     }
     if(sparsity < 1 || sparsity > 100)
     {
-        std::cout<<"Sparsity must be given as an integer ranging from 1 to 100.\n";
-        exit (-2);
+        throw "Sparsity must be given as an integer ranging from 1 to 100.\n";
     }
-}
+    std::cout<<"Input correct"<<std::endl;
+}    //std::vector < std::vector<bool> >test;
+    //auto max_sz = static_cast<long long>(test.max_size());
 
 int main(int argc, char const *argv[])
 {
-    int size, sparsity;
+    long long int size;
+    int sparsity;
     std::string filename;
-    parse_and_check_input(argc, argv, size, sparsity, filename);
+    
+    try
+    {
+        parse_and_check_input(argc, argv, size, sparsity, filename);
+    }
+    catch(const char* e)
+    {
+        std::cout << e;
+        exit(-1);
+    }
+    
     try
     {
         AdjacencyMatrix graph(size, sparsity);
-        graph.print_matrix();
+        if(size <= 50)
+            graph.print_matrix();
         graph.write_to_file(filename);
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
+        exit(-2);
     }
+    
+    
+
     
     return 0;
 }
