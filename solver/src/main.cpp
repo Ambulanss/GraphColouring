918bc9c41256e21b911f6c9f0488d3ac77794e4f
De <ada-977@tlen.pl>
2019-01-07 23:25:20 +0100

    It works with simple instance and one generation

--- solver/src/main.cpp
+++ solver/src/main.cpp
@@ -168,7 +168,7 @@ class genetic_parameters{
 
         genetic_parameters(){}
 
-        genetic_parameters (int pop_size, double p = 0.5, double q = 0.5, double mut_chance = 0.0, 
+        genetic_parameters (int pop_size, double p = 0.25, double q = 0.25, double mut_chance = 0.0, 
                             int par_choosing = 1, int number_of_gen = 100, int breeding_method = 1, 
                             double mut_size = 0.0){
                 population_size = pop_size;
@@ -253,6 +253,21 @@ class individual {
         }
 };
 
+void display_population (std::vector< individual > population, int pop_size, int n){
+    for (int i = 0; i < pop_size; i++){
+        std::cout << i << ".      Fitness: " << population[i].fitness << std::endl;
+        std::cout << "Order:  ";
+        for (int j = 0; j < n; j++){
+            std::cout << population[i].value[j] << " ";
+        }
+        std::cout << "\nColour: ";
+        for (int j = 0; j < n; j++){
+            std::cout << population[i].result_colours[j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
 
 
 
@@ -269,7 +284,7 @@ class parents_pair{
         because it makes a mess. I would leave only n. Because of that, we need to pass n as a constructor
         parameter for parents_pair class I guess, and then set parent_A and parent_B parameters in function
         like "set_parents". */ 
-       
+        //child = individual(n);
         }
 
         void create_child1(std::default_random_engine rng){
@@ -280,46 +295,93 @@ class parents_pair{
             I would add functionality to randomly use parent_A's values at the beginning 
             of the child (before crosspoint) or at the end (after crosspoint).
             */
+            std::cout << "Create child\n";
+            child = individual(n);
             child.initialize_value(); //now the child vector is filled with zeroes
             std::vector <bool> available_values (child.n, true); //should create vector with n true values
+            std::cout << "available values: ";
+            for (int i = 0; i < n; i++){
+                std::cout << available_values[i] << " ";
+            }
+            std::cout << std::endl;
             available_values[0] = false; //we don't have vertex nr 0
             
-            std::vector <int> left_indexes; //we will  use them to know where the gaps are
+            std::vector <int> left_indexes; //we will use them to know where the gaps are
             bool fill_the_gaps = false; //if this is true, we know that there are some gaps (it will fore sure always be true)
             
             int crosspoint;
-            auto dist = std::uniform_int_distribution<int>(1, child.n);
-            crosspoint = dist(rng);
-            
+            std::random_device rd;
+            std::mt19937 mt(rd());
+            auto dist = std::uniform_int_distribution<int>(1, child.n-1);
+            std::cout << "child.n: " << child.n << std::endl;
+            crosspoint = dist(mt);
+            std::cout << "Crosspoint: " << crosspoint << std::endl;
+            std::cout << "Parent_A values: ";
+
+            for (int i = 0; i < parent_A.value.size(); i++)
+                std::cout << parent_A.value[i] << " ";
+            std::cout << "\nParent_B values: ";
+            for (int i = 0; i < parent_B.value.size(); i++)
+                std::cout << parent_B.value[i] << " ";
+            std::cout << std::endl;
+
             for (int i = 1; i < crosspoint; i++){ //we take first values from parent A
                 child.value[i] = parent_A.value[i];
                 available_values[parent_A.value[i]] = false; //we mark used vertex as impossible to use
+                std::cout << "Taking from parent A value " << parent_A.value[i] << " and put it on i = " << i << std::endl;
                 /* reminder: value field contains vertices' numbers, so parent_A.value[2] = 3 means
                 that parent_A is vertex order in which the second vertex processed by greedy algorithm
                 is the third vertex in the input graph */
             }
             for (int i = crosspoint; i < child.n; i++){ //the rest is filled with possible values from parent_B
                 if (available_values[parent_B.value[i]] == true){ //if vertex hasn't been used yet
-                    child.value[i] == parent_B.value[i];    //then insert it into child
+                    child.value[i] = parent_B.value[i];    //then insert it into child
+                    available_values[parent_B.value[i]] = false;
+                    std::cout << "Taking from parent B value " << parent_B.value[i] << " and put it on i = " << i << std::endl;
                 }
                 else {                                  //remember that you have to fill this gap later
                     left_indexes.push_back(i);          //index i has no vertex
                     fill_the_gaps = true;
                 } 
             }
+            
             if (fill_the_gaps){ //if there are some gaps to fill
 
+                std::cout << "left indexes: ";
+                for (int i = 0; i < left_indexes.size(); i++)
+                    std::cout << left_indexes[i] << " ";
+                std::cout << std::endl;
+            
+                std::cout << "Child before filling gaps:\n";
+                for (int i = 0; i < child.value.size(); i++){
+                    std::cout << child.value[i] << " ";
+                }
+                std::cout << std::endl;
                 std::random_shuffle(std::begin(left_indexes), std::end(left_indexes));
                 for (int i = 1; i < child.n; i++){
+                    std::cout << "Im in fatal for.\n";
                     if(available_values[i]){ //if unused vertex is found
-                        child.value[left_indexes[0]] = available_values[i]; 
+                        std::cout << "I found unused value = " << i << std::endl;
+                        child.value[left_indexes[0]] = i; 
+                        std::cout << "child.value[left_indexes[0]] = available_values[i] \n";
+                        std::cout << "left_indexes[0] = " << left_indexes[0] << std::endl;
+                        std::cout << "child.value[left_indexes[0]] = " << child.value[left_indexes[0]] << std::endl;
                         /* assign first available vertex to random gap in child's values - random, because 
                         free left indexes were shuffled. */
                         available_values[i] = false; //set this vertex as used
+                        std::cout << "Now erase first element from left indexes, left it like: \n";
                         left_indexes.erase(std::begin(left_indexes));
+                        for (int i = 0; i < left_indexes.size(); i++){
+                            std::cout << left_indexes[i] << " ";
+                        }
+                        std::cout << std::endl;
                     }
                 }
             }
+            for (int i = 0; i < n; i++){
+                std::cout << child.value[i] << " ";
+            }
+            std::cout << std::endl;
         }
 
         void create_child2(){
@@ -367,11 +429,12 @@ class parents_pair{
 
 class parents {
     public:
-        std::vector <individual> sorted_population; //population sorted by fitness, descending
+        std::vector <individual> sorted_population; //population sorted by fitness, ascending
         std::vector <individual> all_parents;
         int breeders_N;                             //number of parents to choose as the best individuals
         int breeders_M;                             //number of parents to choose as random individuals
         std::default_random_engine rng;
+        int parents_number;
 
         parents(){}
 
@@ -389,7 +452,11 @@ class parents {
                 exit(-1);
             }
             breeders_N = static_cast<int>(floor(p*sorted_population.size()));
+            std::cout << "Breeders_N: " << breeders_N << std::endl;
             breeders_M = static_cast<int>(floor(q*sorted_population.size()));
+            std::cout << "Breeders_M: " << breeders_M << std::endl;
+            parents_number = breeders_N + breeders_M;
+            std::cout << "Parents number: " << parents_number << std::endl;
         }
 
         void choose1(){
@@ -401,9 +468,14 @@ class parents {
             auto dist = std::uniform_int_distribution<int>(breeders_N, static_cast<int>(sorted_population.size()));
             for (int i = 0; i < breeders_M; i++){
                 temp = dist(rng);
+                std::cout << "[Choose1] random index: " << temp << std::endl;
                 all_parents.push_back(sorted_population[temp]);
             }
-            std::shuffle(std::begin(all_parents), std::end(all_parents), rng); //suffle all parents
+            std::cout << "Unshuffled parents: \n";
+            display_population(all_parents, parents_number, sorted_population[1].n);
+            std::random_shuffle(std::begin(all_parents), std::end(all_parents)); //suffle all parents
+            std::cout << "Shuffled parents:   \n";
+            display_population(all_parents, parents_number, sorted_population[1].n);
         }
 
         void choose2(){
@@ -433,6 +505,7 @@ class genetic_algorithm {
                             //but it may have some references in other classes - that has to be checked 
         int n; //number of vertices + 1 - size of graph adjacency matrix - also in params, but I would leave it here for comfort
         std::vector< individual > population; //vector of vectors with vertices' order
+        std::vector< individual > new_population;
         std::vector<std::vector<bool> > matrix; //graph
         genetic_parameters params; //parameters, I hope it would be nice to set all parameters in one place
                                     //and then just run solver
@@ -462,18 +535,46 @@ class genetic_algorithm {
         }
 
         void choose_parents(){
-            parents chosen_parents(params.breeders_p, params.breeders_q, population, rng); 
+            chosen_parents = parents(params.breeders_p, params.breeders_q, population, rng); 
             if (params.parents_choosing == 1){ //I plan to have more than one parents choosing method
                 chosen_parents.choose1();
             }
             else {
                 chosen_parents.choose2();
             }
+            for (int i = 0; i < chosen_parents.all_parents.size(); i++){
+                new_population.push_back(chosen_parents.all_parents[i]);
+                std::cout << "wpycham element do new_population, i = " << i << std::endl;
+            }
         }
 
 
         void create_children(){
             //create as many children as rejected individuals
+            int size = chosen_parents.all_parents.size();
+            int goal = params.population_size - new_population.size();
+            std::vector<parents_pair> pairs;
+            std::cout << "size = " << size << std::endl;
+            std::cout << "size/2 = " << size/2 << std::endl;
+            for (int i = 0; i < size/2; i++){
+                pairs.push_back(parents_pair(chosen_parents.all_parents[i], chosen_parents.all_parents[size-1-i], n));
+                std::cout << "creating pairs, i = " << i << std::endl;
+            }
+            int i = 0;
+            std::cout << "Create children dupa\n" << std::endl;
+            while (goal--){
+                std::cout << "i = " << i << std::endl;
+                pairs[i].create_child1(rng);
+                new_population.push_back(pairs[i].child);
+                i++;
+                if (i == pairs.size()){
+                    std::cout << "Reseting i\n" << std::endl;
+                    i = 0;
+                }
+            }
+            if (new_population.size() != population_size) {
+                std::cout << "Size of new population is incorrect" << std::endl;
+            }
             /* 
             Number of children for pair is:
             (population_size - number_of_parents) / (number_of_parents/2)
@@ -538,21 +639,7 @@ class genetic_algorithm {
 
 };
 
-void display_population (std::vector< individual > population, int pop_size, int n){
-    for (int i = 0; i < pop_size; i++){
-        std::cout << i << ".      Fitness: " << population[i].fitness << std::endl;
-        std::cout << "Order:  ";
-        for (int j = 0; j < n; j++){
-            std::cout << population[i].value[j] << " ";
-        }
-        std::cout << "\nColour: ";
-        for (int j = 0; j < n; j++){
-            std::cout << population[i].result_colours[j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
-}
+
 
 void display_parameters (genetic_parameters params){
     std::cout << "Population size: " << params.population_size << std::endl;
@@ -607,6 +694,15 @@ int main(int argc, char const *argv[])
     std::cout << "----- Sorted population -----\n";
     display_population(genetic.population, params.population_size, matrix.size());
 
+    /* Choose and display parents */
+    genetic.choose_parents();
+
+    /* Create children */
+    genetic.create_children();
+    for (int i = 0; i < params.population_size; i++){
+        genetic.new_population[i].count_fitness(matrix);
+    }
+    display_population(genetic.new_population, params.population_size, matrix.size());
 
 
     return 0;
